297873094464598aa0063a1f76c7470606828d84
kaoiks354 <kox354@gmail.com>
2023-07-24 18:50:07 +0200

    Added test for accounts

--- src/ams/tests/test_api.py
+++ src/ams/tests/test_api.py
@@ -12,3 +12,25 @@ def test_task_create(client):
     assert response.status_code == 200
 
     assert len(response.data) == 1
+
+
+@pytest.mark.django_db
+def test_account_create(client):
+    data = {'currency': 'USD'}
+    response = client.post('/api/accounts', data, format='json')
+    assert response.status_code == 201
+
+    response = client.get('/api/accounts')
+    assert response.status_code == 200
+
+    assert len(response.data) == 1
+    account_id = response.data[0]['id']
+
+    response = client.delete(f'/api/accounts/{account_id}')
+    assert response.status_code == 204
+
+    response = client.get('/api/accounts')
+    assert response.status_code == 200
+
+    assert len(response.data) == 0
+
