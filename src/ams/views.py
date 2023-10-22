3bde2c56b5551f059da0ef6da57094cbbb6c5bc1
JW <kox354@gmail.com>
2023-10-22 23:59:10 +0200

    Rebuild account balance for all currencies

--- src/ams/views.py
+++ src/ams/views.py
@@ -12,30 +12,49 @@ from .permissions import IsObjectOwner
 logger = logging.getLogger(__name__)
 
 
-def rebuild_account_balance(account_id, currency, transaction_date):
-    account_history = models.AccountHistory.objects.filter(account_id=account_id,
-                                                           date=transaction_date - timedelta(days=1)).first()
-    account_balance = models.AccountBalance.objects.filter(account_id=account_id,
-                                                           currency=currency).first()
-    if account_history:
-        account_balance_history = models.AccountHistoryBalance.objects.filter(account_history__id=account_history.id, currency=currency).first()
-        if account_balance_history:
-            account_balance.amount = account_balance_history.amount
-        else:
-            account_balance.amount = 0
+def add_transaction_to_account_balance(transaction, account, account_balance):
+    if transaction.type == 'deposit':
+        account_balance.amount += transaction.amount
+    elif transaction.type == 'withdrawal':
+        account_balance.amount -= transaction.amount
+
+    if account.last_transaction_date:
+        if transaction.date > account.last_transaction_date:
+            account.last_transaction_date = transaction.date
     else:
-        account_balance.amount = 0
+        account.last_transaction_date = transaction.date
+
+    account_balance.save()
+    account.save()
+
+
+def rebuild_account_balance(account, transaction_date):
+    account_history = models.AccountHistory.objects.filter(account_id=account.id,
+                                                           date=transaction_date - timedelta(days=1)).first()
+
+    account_balances = models.AccountBalance.objects.filter(account_id=account.id)
+    account_balances_by_currency = {account_balance.currency: account_balance for account_balance in account_balances}
+    currencies = account_balances_by_currency.keys()
+
+    account_balance_histories = models.AccountHistoryBalance.objects.filter(account_history__id=account_history.id)
+    account_balance_histories_by_currency = {account_balance_history.currency: account_balance_history
+                                             for account_balance_history in account_balance_histories}
+
+    for currency in currencies:
+        if currency in account_balance_histories_by_currency:
+            account_balances_by_currency[currency].amount = account_balance_histories_by_currency[currency].amount
+        else:
+            account_balances_by_currency[currency].amount = 0
 
     desired_date = date(transaction_date.year, transaction_date.month, transaction_date.day)
 
     transactions_on_date = models.Transaction.objects.filter(date__date=desired_date).order_by('date')
-    for transaction in transactions_on_date:
-        if transaction.type == models.Transaction.DEPOSIT:
-            account_balance.amount += transaction.amount
-        elif transaction.type == models.Transaction.WITHDRAWAL:
-            account_balance.amount -= transaction.amount
 
-    account_balance.save()
+    for transaction in transactions_on_date:
+        add_transaction_to_account_balance(transaction, account, account_balances_by_currency[transaction.currency])
+
+    for balance in account_balances:
+        balance.save()
 
 
 class AccountViewSet(viewsets.ModelViewSet):
@@ -92,31 +111,13 @@ class TransactionViewSet(viewsets.ViewSet):
             )
 
         if account.last_save_date and account.last_transaction_date:
-            print("")
             if account.last_transaction_date > transaction.date > account.last_save_date:
-                rebuild_account_balance(account_id, transaction.currency, transaction.date)
+                rebuild_account_balance(account, transaction.date)
             else:
-                if transaction.type == 'deposit':
-                    account_balance.amount += transaction.amount
-                elif transaction.type == 'withdrawal':
-                    account_balance.amount -= transaction.amount
-                if account.last_transaction_date:
-                    if transaction.date > account.last_transaction_date:
-                        account.last_transaction_date = transaction.date
-                else:
-                    account.last_transaction_date = transaction.date
+                add_transaction_to_account_balance(transaction, account, account_balance)
         else:
-            if transaction.type == 'deposit':
-                account_balance.amount += transaction.amount
-            elif transaction.type == 'withdrawal':
-                account_balance.amount -= transaction.amount
-            if account.last_transaction_date:
-                if transaction.date > account.last_transaction_date:
-                    account.last_transaction_date = transaction.date
-            else:
-                account.last_transaction_date = transaction.date
-            account_balance.save()
-        account.save()
+            add_transaction_to_account_balance(transaction, account, account_balance)
+
         return Response({"msg": "Transaction created."}, status=status.HTTP_201_CREATED)
 
     def list(self, request, account_id):
