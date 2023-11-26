3d0d409513b1ee854e7c0b0410714b7a530af772
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-11-26 19:25:10 +0100

    fix: Deleting correlated account transaction

--- src/ams/services/stock_balance_service.py
+++ src/ams/services/stock_balance_service.py
@@ -210,9 +210,10 @@ def modify_stock_transaction(stock_transaction, old_stock_transaction_date):
 @transaction.atomic
 def delete_stock_transaction(stock_transaction):
     stock_balance = models.StockBalance.objects.get(isin=stock_transaction.isin, account=stock_transaction.account)
+    stock_transaction_id = stock_transaction.id
     stock_transaction.delete()
     rebuild_stock_balance(stock_balance, stock_transaction.date.date())
 
-    if models.Transaction.objects.filter(correlation_id=stock_transaction.id).exists():
-        account_transaction = models.Transaction.objects.get(correlation_id=stock_transaction.id)
+    if models.Transaction.objects.filter(correlation_id=stock_transaction_id).exists():
+        account_transaction = models.Transaction.objects.get(correlation_id=stock_transaction_id)
         account_balance_service.delete_transaction(account_transaction)
